5f227464df65b176baf3b0c5021fe544f174b0a3
Datnguyen <datndd2604@gmail.com>
2022-04-09 15:12:08 +0700

    add floatingActionBottom

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:netflix_clone/components/bottom_nav_bar.dart';
+import 'package:netflix_clone/constants.dart';
 
 class Index extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: SvgPicture.asset('assets/icons/movie.svg'),
+        backgroundColor: kPrimaryColor,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
